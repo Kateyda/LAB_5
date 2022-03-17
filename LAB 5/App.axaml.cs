87cb55e345e1d4e6083eb4cba862e95002016a3e
Kateyda <78633772+Kateyda@users.noreply.github.com>
2022-03-17 18:55:14 +0700

    Добавьте файлы проекта.

---
+++ LAB 5/App.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using LAB_5.ViewModels;
+using LAB_5.Views;
+
+namespace LAB_5
+{
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow
+                {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
